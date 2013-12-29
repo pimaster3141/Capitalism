4e942d3ef5b2e649e85ddc98195647c5712b3f82
Jason Yang <pimaster3141@gmail.com>
2013-12-29 12:26:40 -0500

    added a bit of meat to moves... nothing really noticable

--- src/game/Move.java
+++ src/game/Move.java
@@ -1,15 +1,37 @@
 package game;
 
+import java.util.ArrayList;
+
+import cards.Card;
+import player.Player;
+
 /**
  * Object representing a 'move' becasue objects are much easier to deal with than strings 
  * contains helper methods to compare different moves and classify the type of move 
  *
+ * moves are immutable
  */
 public class Move implements Comparable<Move>
 {
+	private final Player player;
+	private final ArrayList<Card> cards;
 	
+	public Move(Player player, ArrayList<Card> cards)
+	{
+		this.player = player;
+		this.cards = new ArrayList<Card>(cards);
+	}
 	
+	public ArrayList<Card> getCards()
+	{
+		return new ArrayList<Card>(cards);
+	}
 
+	public Player getPlayer()
+	{
+		return player;
+	}
+	
 	public int compareTo(Move arg0)
 	{
 		// TODO Auto-generated method stub
